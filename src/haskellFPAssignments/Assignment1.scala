163ffa9acad37df81486916bb4d63a98b740b246
Aaron Cronin <cronin@include.cat>
2013-11-11 18:26:11 +0000

    Question 2

--- src/haskellFPAssignments/Assignment1.scala
+++ src/haskellFPAssignments/Assignment1.scala
@@ -9,7 +9,7 @@ object Assignment1 {
     println(halveEvensHigher(halveInput))
   }
 
-  /** Returning a list of half of all the even input elements */
+  /** 1: Returning a list of half of all the even input elements */
 
   /** List Comprehension */
   def halveEvensComp(nums: List[Int]): List[Int] = {
@@ -21,11 +21,11 @@ object Assignment1 {
   def halveEvensRec(nums: List[Int]): List[Int] = {
     def recStep(acc: ListBuffer[Int], left: List[Int]): List[Int] = {
       left match {
-        case List() => acc.toList
+        case List()  => acc.toList
         case x :: xs => if (x % 2 == 0)
-          recStep(acc += x / 2, xs)
-        else
-          recStep(acc, xs)
+                            recStep(acc += x / 2, xs)
+                        else
+                            recStep(acc, xs)
       }
     }
     recStep(ListBuffer(), nums)
@@ -34,5 +34,32 @@ object Assignment1 {
   /** Higher-order functions */
   def halveEvensHigher(nums: List[Int]): List[Int] = {
     nums.filter(x => x % 2 == 0).map(x => x / 2)
+  }
+  
+  /** 2: Returning all numbers in the input list between two bounds (inclusive) */
+  
+  /** List Comprehension */
+  def inRangeComp(nums: List[Int], lower: Int, upper: Int): List[Int] = {
+    for (i <- nums if lower until upper contains i)
+      yield i
+  }
+  
+  /** Recursion */
+  def inRangeRec(nums: List[Int], lower: Int, upper: Int): List[Int] = {
+    def recStep(acc: ListBuffer[Int], left: List[Int]): List[Int] = {
+      left match {
+        case List()  => acc.toList
+        case x :: xs => if (lower until upper contains x)
+                            recStep(acc += x, xs)
+                        else
+                            recStep(acc, xs)
+      }
+    }
+    recStep(ListBuffer(), nums)
+  }
+  
+  /** Higher-order functions */
+  def inRangeHigher(nums: List[Int], lower: Int, upper: Int): List[Int] = {
+    nums.filter(x => lower until upper contains x)
   }
 }
