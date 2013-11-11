ff59573d6f428d85211b7e443bf89c2b1fe053d5
Aaron Cronin <cronin@include.cat>
2013-11-11 19:12:33 +0000

    Question 4

--- src/haskellFPAssignments/Assignment1.scala
+++ src/haskellFPAssignments/Assignment1.scala
@@ -81,4 +81,34 @@ object Assignment1 {
     def countPositivesHigher(nums: List[Int]): Int ={
         nums.filter(x => x > 0).map(_ => 1).sum
     }
+    
+    /** 4: Professor Pennypincher will not buy anything if he has to pay more than £199.00.
+     *  But, as a member of the Generous Teachers Society, he gets a 10% discount on anything he buys.
+     *  Write a function pennypincher that takes a list of prices and returns the total amount that
+     *  Professor Pennypincher would have to pay,
+     *  if he bought everything that was cheap enough for him. */
+    var pincherLimit = 199.0
+    
+    /** List Comprehension */
+    def pennyPincherComp(prices: List[Double]): Double = {
+        (for (price <- prices if price * 0.9 <= pincherLimit) yield price * 0.9).sum
+    }
+    
+    /** Recursion */
+    def pennyPincherRec(prices: List[Double]): Double = {
+        def recStep(acc: Double, rest: List[Double]): Double = {
+            rest match {
+                case List()  => acc
+                case x :: xs => var price = x * 0.9
+                                if (price <= pincherLimit) recStep(acc + price, xs)
+                                else                         recStep(acc, xs)
+            }
+        }
+        recStep(0.0, prices)
+    }
+    
+    /** Higher-order Functions */
+    def pennyPincherHigher(prices: List[Double]): Double = {
+        prices.map(x => x * 0.9).filter(x => x <= pincherLimit).sum
+    }
 }
